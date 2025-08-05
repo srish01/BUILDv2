1ef8a40491aa272247e22b2e78f684299cb15ba6
dangioni <angioni.daniele97.00@gmail.com>
2025-08-05 12:13:42 +0200

    modified pass bash script

--- run.sh
+++ run.sh
@@ -16,4 +16,4 @@ folder="pass_cifar10-5T"
 
 cmd="nohup python run.py --train --framework $framework --model $model --n_tasks $n_tasks --dataset $dataset --adapter_latent $adapter_latent --optim $optim --n_epochs $n_epochs --lr $lr --batch_size $batch_size --class_order $class_order --folder $folder --cuda_id $cuda"
 
-$cmd  --kd_weight 1 --protoAug_weight 10 --temp 0.1 --use_clip_grad --clip_grad 10 &> nohups/nohup_${folder}.out &
+$cmd  --kd_weight 10 --protoAug_weight 10 --temp 0.1 --use_clip_grad --clip_grad 10 &> nohups/nohup_${folder}.out &
